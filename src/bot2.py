b9ab55441c9220240ba55516c5c99371bed94d2f
Mateus M. A. Cabral <mateuscabralalves@outlook.com>
2022-06-04 16:59:46 -0300

    Update 'collect_closing_quotation' method

--- src/bot2.py
+++ src/bot2.py
@@ -2,25 +2,41 @@ from bot1 import html_table_to_dataframe
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-def collect_dividend_yield():
+def collect_closing_quotation():
+    # This method will return a dictionary, where the key will be the
+    # 'FII' code, and the value the closing quotation of that 'FII'
+
+    fii_closing_quotation_dict = {}
     fii_code_list = html_table_to_dataframe()
-    test_date = '01/2022'
-
-    url = 'https://bvmf.bmfbovespa.com.br/SIG/FormConsultaMercVista.asp?strTipoResumo=RES_MERC_VISTA&strSocEmissora={fii_name}&strDtReferencia={date}&strIdioma=P&intCodNivel=2&intCodCtrl=160'
-    url = url.format(fii_name=fii_code_list[0], date=test_date)
+    # Must be MM/YYYY
+    date = '05/2022'
 
     # Setting up browser
     options = Options()
     options.headless = True
     browser = webdriver.Firefox(options=options)
 
-    # Accessing url
+    # The B3's stores the closing quotation in the following url
+    url = 'https://bvmf.bmfbovespa.com.br/SIG/FormConsultaMercVista.asp?strTipoResumo=RES_MERC_VISTA&strSocEmissora={fii_name}&strDtReferencia={date}&strIdioma=P&intCodNivel=2&intCodCtrl=160'
+
+    # -------------------------------------------------------------------------------
+    # By using a placeholder, the bot can search for the
+    # desired 'FII' at the desired date
+    url = url.format(fii_name=fii_code_list[0], date=date)
+
+    # Accessing formatted url
     browser.get(url)
 
-    # Code for test purposes
-    value = browser.find_element(
-        By.XPATH, '/html/body/table[3]/tbody/tr/td/table/tbody/tr/td/table[1]/tbody/tr[2]/td/table/tbody/tr[23]/td[12]')
+    try:
+        value = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="tblResDiario"]/tbody/tr[2]/td/table'))
+        )
+    except:
+        print("Error!")
 
-    print(value.get_attribute('outerHTML'))
+    # Code for test purposes
--- src/main.py
+++ src/main.py
@@ -3,4 +3,4 @@ import bot2
 
 # bot1.scraping_html()
 # bot1.html_table_to_dataframe()
-bot2.collect_dividend_yield()
+bot2.collect_closing_quotation()
